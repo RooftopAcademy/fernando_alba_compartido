83e140747ca3f602083313351b710b119c913d77
Fernando Alba <fernando.alba@live.com.ar>
2021-09-20 23:11:55 -0300

    feat: add alt text on img-brand with js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,6 @@ store.fetchBrands()
 
 Array.from(document.getElementsByClassName('js-brand-list'))
     .forEach(list => {
-        console.log(store.catalog.brandList);
         store.catalog.brandList.forEach(brand => {
             list.innerHTML += brandItemView(brand)
         })
